8d18226f198d7e5ecd609c56760ac9f22cb4a9a3
Nitesh <05niteshraj@gmail.com>
2022-09-04 12:46:11 +0530

    Ready for testing

--- handler.js
+++ handler.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-//AWS dynamodb connection
+//AWS dynamodb and ses connection
 const AWS = require("aws-sdk");
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const dynamoDb = new AWS.DynamoDB.DocumentClient(); 
+const ses = new AWS.SES(); // Simple Email Service
 
 //Get Api
 module.exports.getquestion = (event, context, callback) => {
-
   //defining paramaters
   const params = {
     TableName: process.env.QUESTION_TABLE,
@@ -39,20 +39,24 @@ module.exports.getquestion = (event, context, callback) => {
     };
     callback(null, response);
   });
-}
+};
 
 //Create Api
 module.exports.createquestion = (event, context, callback) => {
-  
   //collecting data from event
   const data = JSON.parse(event.body);
   
   //content to put into dynomodb
-  const timestamp = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
+  //Timestamp for +5:30 time
+  const timestamp = new Date().toLocaleString("en-US", {
+    timeZone: "Asia/Kolkata",
+  });
+  //Uuid --> Uniq User Id <-- for both table
+  const qid = AWS.util.uuid.v4();
   const params = {
     TableName: process.env.QUESTION_TABLE,
     Item: {
-      Qid: AWS.util.uuid.v4(),
+      Qid: qid,
       Quser: data.Quser,
       Qtitle: data.Question,
       Qdetails: data.Qdetail,
@@ -61,6 +65,22 @@ module.exports.createquestion = (event, context, callback) => {
     },
   };
 
+  const param_mail = {
+    TableName: process.env.MAIL_TABLE,
+    Item: {
+      Qid: qid,
+      Quser: data.Quser,
+      Mail: data.mail,
+    },
+  };
+  // Putting value at User table --> Mail Table
+  dynamoDb.put(param_mail, (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+
+  //Putting data into Data Table
   dynamoDb.put(params, (error) => {
     // handle potential errors
     if (error) {
@@ -84,11 +104,10 @@ module.exports.createquestion = (event, context, callback) => {
     };
     callback(null, response);
   });
-}
+};
 
 //Scan Api
 module.exports.allquestions = (event, context, callback) => {
-  
   //define parameters
   const params = {
     TableName: process.env.QUESTION_TABLE,
@@ -117,14 +136,63 @@ module.exports.allquestions = (event, context, callback) => {
     };
     callback(null, response);
   });
-}
+};
 
 //Answer Api
 module.exports.answerquestion = (event, context, callback) => {
-
   //collecting data
   const data = JSON.parse(event.body);
-  
+
+  const param_mail = {
+    TableName: process.env.MAIL_TABLE,
+    Key: {
+      Qid: event.pathParameters.Qid,
+    },
+  };
+
+  // Getting data from Mail-Table for SES => For sending mail
+  dynamoDb.get(param_mail, (error, result) => {
+    if (error) {
+      console.log(error);
+    }
+    
+    //SES part
+    const subject = "You Got Your Answer"
+    const message = `
+    
+    Hola ${result.Quser} 
+    
+    Your Question : ${data.Qtitle}
+
+    We got an Answer From : ${data.Ansuser}
+
+    Visit our site to see the answer
+    
+    Heppy Learning :)
+    `;
+   
+    // sending mail id <-- verified at aws ses <-- you need to verifed it first
+    const from = "nit.pc058@gmail.com";
+    const param_ses = {
+      Destination: {
+        ToAddresses: ["nitpc.strat@gmail.com"],//mail from table => [result.Mail],
+      },
+      Message: {
+        Body: {
+          Text: { Data: message },
+        },
+        Subject: { Data: subject },
+      },
+      Source: from,
+    };
+    try {
+      ses.sendEmail(param_ses).promise();
+      console.log("done Mail");
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
   //defining params
   const params = {
     TableName: process.env.QUESTION_TABLE,
@@ -140,8 +208,8 @@ module.exports.answerquestion = (event, context, callback) => {
     },
   };
 
-  // Here put will overwrite item in table 
-  // simply put this new item in place of the old item with same Qid 
+  // Here put will overwrite item in table
+  // simply put this new item in place of the old item with same Qid
   dynamoDb.put(params, (error) => {
     // handle potential errors
     if (error) {
@@ -165,5 +233,4 @@ module.exports.answerquestion = (event, context, callback) => {
     };
     callback(null, response);
   });
-
-}
+};
